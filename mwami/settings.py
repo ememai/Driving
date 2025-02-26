a9cdcb1ccd23790a73e04ef0805b9f4aeab91f6f
MWAMI <modesteihirwemwami@gmail.com>
2025-02-26 08:46:43 +0200

    feat: update virtual environment configuration, enhance .gitignore, and remove unused image file

--- mwami/settings.py
+++ mwami/settings.py
@@ -115,10 +115,7 @@ WSGI_APPLICATION = 'mwami.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': dj_database_url.config(default=config('DB_URL'))
 }
 
 AUTHENTICATION_BACKENDS = [
