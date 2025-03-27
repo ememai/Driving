3567e42e596597804d445f60eec2fb9f59b8b466
MWAMI <modesteihirwemwami@gmail.com>
2025-03-27 13:00:39 +0200

    style: update admin CSS for improved layout and add custom JavaScript for image selection

--- staticfiles/admin/js/custom_admin.js
+++ staticfiles/admin/js/custom_admin.js
@@ -5,27 +5,42 @@ document.addEventListener('DOMContentLoaded', function () {
     chooseImageButtons.forEach(button => {
         button.addEventListener('click', function () {
             const choice = this.getAttribute('data-choice'); // Get the associated choice (e.g., "choice1")
-            const radioGroupWrapper = document.querySelector(`#id_${choice}_signs`); // Target the parent <div>
-            const radioInputs = radioGroupWrapper.querySelectorAll('input.choice-sign-radio'); // Target all <input> elements inside the parent <div>
+            const radioQuestionImages = document.querySelector(`#id_${choice}_sign`);
+            const radioQuestionInputs = radioQuestionImages ? radioQuestionImages.querySelectorAll('input') : null;
 
-            console.log('Button clicked:', this);
-            console.log('Associated choice:', choice);
-            console.log('Radio group wrapper:', radioGroupWrapper);
-            console.log('Radio inputs:', radioInputs);
+            if (radioQuestionImages) {
+                // Toggle the 'hidden' class on the parent <div>
+                radioQuestionImages.classList.toggle('hidden');
+                radioQuestionImages.classList.toggle('custom-flex');
+                console.log('Toggled hidden class on wrapper:', radioQuestionImages.classList);
+            }
+
+            if (radioQuestionInputs && radioQuestionInputs.length > 0) {
+                // Loop through all <input> elements and toggle the 'hidden' class
+                radioQuestionInputs.forEach(input => {
+                    input.classList.toggle('hidden');
+                    console.log('Toggled hidden class on input:', input.classList);
+                });
+            }
+
+            const radioGroupWrapper = document.querySelector(`#id_${choice}_signs`);
+            const radioInputs = radioGroupWrapper ? radioGroupWrapper.querySelectorAll('input.choice-sign-radio') : null;
 
             if (radioGroupWrapper) {
                 // Toggle the 'hidden' class on the parent <div>
                 radioGroupWrapper.classList.toggle('hidden');
+                radioGroupWrapper.classList.toggle('custom-flex');
                 console.log('Toggled hidden class on wrapper:', radioGroupWrapper.classList);
             } else {
                 console.error(`Radio group wrapper for choice "${choice}" not found.`);
             }
 
-            if (radioInputs.length > 0) {
+            if (radioInputs && radioInputs.length > 0) {
                 // Loop through all <input> elements and toggle the 'hidden' class
                 radioInputs.forEach(input => {
-                    input.classList.toggle('hidden');
-                    console.log('Toggled hidden class on input:', input.classList);
+                  input.classList.toggle('hidden');
+                    
+                  console.log('Toggled hidden class on input:', input.classList);
                 });
             } else {
                 console.error(`No radio inputs found for choice "${choice}".`);
